59be966b210c64a895093278a2f6faa4bf8f5c08
craig <cbrookes@redhat.com>
2017-03-22 15:11:59 +0000

    bump

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ go build .
 cd ..
 cd ..
 imagehash=`docker build -q .`
-docker tag $imagehash feedhenry/negotiator:0.0.5
+docker tag $imagehash feedhenry/negotiator:0.0.6
 oc deploy --latest negotiator 
 
--- pkg/openshift/templates/data-mongo.config_job.json.tpl.go
+++ pkg/openshift/templates/data-mongo.config_job.json.tpl.go
@@ -21,7 +21,7 @@ var DataMongoConfigJob = `
         "containers": [
           {
             "name": "datamongoconfig",
-            "image": "feedhenry/negotiator:0.0.5",
+            "image": "feedhenry/negotiator:0.0.6",
             "command": ["jobs",	
               "datamongoconfig",
 			  "--admin-user={{if isset . "admin-user"}}{{ index . "admin-user"}}{{end}}",
